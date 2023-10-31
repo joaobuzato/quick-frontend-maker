54b94e338e9897a666b2b7c1cd01b5466b1f7581
João Pedro Buzato <joaobuzato@gmail.com>
2023-10-31 20:58:38 -0300

    adding get

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,65 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+
 import "./App.css";
 import ItemTable from "./components/UI/ItemTable";
-import { url } from "inspector";
 
 function App() {
-  const [items, setItems] = useState([]);
+  const [table, setTable] = useState(<></>);
+  const [isTableOpen, setIsTableOpen] = useState(false);
+  const [urlValue, setUrlValue] = useState("");
 
   const getFromServer = async (url: string) => {
     const response = await fetch(url);
     return await response.json();
   };
-  const url = "http://localhost:8080/actions";
 
-  useEffect(() => {
-    getFromServer(url).then((response) => {
-      console.log(response);
-      setItems(response);
-    });
-  }, []);
+  const submitHandler = async (event: any) => {
+    event.preventDefault();
+
+    getFromServer(urlValue)
+      .then((response) => {
+        openTable(urlValue, response);
+      })
+      .catch(() => {
+        alert("deu merda");
+      });
+  };
+
+  const openTable = (url: string, items: any) => {
+    setIsTableOpen(true);
+    setTable(
+      <>
+        <h1>{url}</h1>
+        <ItemTable onReturnCallback={closeTable} data={items}></ItemTable>
+      </>
+    );
+  };
+
+  const closeTable = () => {
+    setTable(<></>);
+    setIsTableOpen(false);
+  };
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUrlValue(event.currentTarget.value);
+  };
 
   return (
     <div className="App">
-      <h1>{url}</h1>
-      <ItemTable data={items}></ItemTable>
+      {isTableOpen ? (
+        <>{table}</>
+      ) : (
+        <form onSubmit={submitHandler}>
+          <input
+            placeholder="url"
+            id="url"
+            type="text"
+            value={urlValue}
+            onChange={changeHandler}
+          ></input>
+          <button type="submit">Fazer Chamada</button>
+        </form>
+      )}
     </div>
   );
 }
--- src/components/UI/ItemTable.tsx
+++ src/components/UI/ItemTable.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 
 interface DataItem {
-  id: number;
   [key: string]: any;
 }
 
 interface Props {
   data: DataItem[];
+  onReturnCallback: Function;
 }
 
-function ItemTable({ data }: Props) {
+function ItemTable({ data, onReturnCallback }: Props) {
+  let columns: string[] = [];
   if (!data || data.length === 0) {
-    return <div>No data to display</div>;
+    return (
+      <>
+        <div>No data to display</div>
+        <button onClick={() => onReturnCallback()}>Voltar</button>
+      </>
+    );
   }
-
-  const columns = Object.keys(data[0]);
+  if (!Array.isArray(data)) {
+    columns = Object.keys(Array.from(data));
+  } else {
+    columns = Object.keys(data[0]);
+  }
 
   return (
     <div>
@@ -28,17 +37,17 @@ function ItemTable({ data }: Props) {
           </tr>
         </thead>
         <tbody>
-          {data.map((item) => (
-            <tr key={item.id}>
+          {Array.from(data).map((item) => (
+            <tr key={item[columns[0]]}>
               {columns.map((column) => {
-                if (!Array.isArray(item[column]))
-                  return <td key={column}>{String(item[column])}</td>;
-                else return <td>Array</td>;
+                if (Array.isArray(item[column])) return <td>Array</td>;
+                return <td key={column}>{String(item[column])}</td>;
               })}
             </tr>
           ))}
         </tbody>
       </table>
+      <button onClick={() => onReturnCallback()}>Voltar</button>
     </div>
   );
 }
